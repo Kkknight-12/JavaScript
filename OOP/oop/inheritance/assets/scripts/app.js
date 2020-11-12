2ca0f361f6a9c04830e4f3836a63a35e2a2fd625
Knight <mayanks365@gmail.com>
2020-11-12 23:00:01 +0530

    practice

--- OOP/oop/inheritance/assets/scripts/app.js
+++ OOP/oop/inheritance/assets/scripts/app.js
@@ -14,10 +14,10 @@ class Product {
 }
 
 // 
-class ElementAttributes {
+class ElementAttribute {
     constructor( attrName, attrValue ){
         this.name = attrName;
-        this.hook = attrValue;
+        this.value = attrValue;
     }
 }
 
@@ -35,7 +35,7 @@ class Component {
         }
         if( attributes && attributes.length > 0 ) {
             for( const attr of attributes ){
-                rootElement.setAttribute( attr.name, attr.value );
+                rootElement.setAttribute(attr.name, attr.value);
             }
         }
         document.getElementById(this.hookId).append(rootElement);
@@ -44,9 +44,10 @@ class Component {
     }
 }
 
-
 // 
-class ShoppingCart extends Component{
+// 
+class ShoppingCart 
+    extends Component {
     items = [];
 
     set cartItems( value ) {
@@ -89,102 +90,92 @@ class ShoppingCart extends Component{
 };
 
 // Item Class
-class ProductItem {
-    constructor( product ) {
+class ProductItem
+    extends Component {
+
+    constructor(product, renderHookId) {
+        super(renderHookId);
         this.product = product;
     }
 
     addToCart() {
         App.addProductToCart( this.product );
-        // console.log( 'Adding product to card....' );
-        // console.log( this.product );
-        // console.log( this.product.title );
     }
 
     render() {
-         const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-            <div class="container">
-                <img src="${this.product.imageUrl}" alt="${this.product.title}" />
-                <div
-                class="product-item_content">
-                    <h2>${this.product.title}</h2>
-                    <p>${this.product.description}</p>
-                    <h3>\$${this.product.price}</h3>
-                    <div class="button-center">
-                    <button>Add to Cart</button>
-                    </div>
+        const prodEl = this.createRootElement('li', 'product-item');
+    
+        prodEl.innerHTML = `
+        <div class="container">
+            <img src="${this.product.imageUrl}" alt="${this.product.title}" />
+            <div
+            class="product-item_content">
+                <h2>${this.product.title}</h2>
+                <p>${this.product.description}</p>
+                <h3>\$${this.product.price}</h3>
+                <div class="button-center">
+                <button>Add to Cart</button>
                 </div>
             </div>
+        </div>
         `;
 
         const addCardButton = prodEl.querySelector('button');
-        // adding product to function addToCart
-        //                               ProductItem.addToCart(product)
         addCardButton.addEventListener( 'click', this.addToCart.bind(this) );
-        return prodEl;
+
     }
 }; 
 
 // List of all Products
-class ProductList {
+class ProductList 
+    extends Component {
+    constructor(renderHookId) {
+    super(renderHookId);
+    }
+
     products = [ 
-
-        new Product (
-        
+        new Product (  
             'A  Pillow',
-
             'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcR4rUSM1IY6-u3CW332UCRP5fZ7hnbzMZyfMZNeyhMhWe3KeP3YR6y4lEkkOORT5oo_utkpX0XzaOZTZ6TW2KmniprQ4DrBuEgb_S5KqAo8d3MzwJjUQcgw&usqp=CAE',
             'A soft pillow!',
             19.99
         ),
 
        new Product (
-
             'A Carpet',
-
             'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQGOGVv80DZW3qNCUnzp4krvrtUz1fwA_t_qvCjDO4LML30v4N0cCauKnsfASoRyL1RTxN2OeZd8_ahxnw_rMkEVjVltNAel_hzWnavGfk&usqp=CAE',
             "A carpet which you might like - or not.",
             89.99,       
         )   
     ];
 
-    constructor () { }
-
     render() {
-        const prodList = document.createElement('ul');
-        prodList.className = 'product-list';
+
+        this.createRootElement('ul', 'product-list', 
+        [new ElementAttribute('id', 'prod-list')]);
 
         for ( const prod of this.products ) {
 
-        const productItem = new ProductItem( prod );
-        const prodEl = productItem.render();
-        prodList.append(prodEl);
-
+            const productItem = new ProductItem(prod, 'prod-list');
+            productItem.render();
         }   
-        return prodList;
     }
 };
 
+
 class  Shop {
     render() {
-        const renderHook = document.getElementById('app');
 
         this.cart = new ShoppingCart('app');
         this.cart.render();
 
-        const productList  = new ProductList();
-        const prodListEl = productList.render()
-
-        renderHook.append(prodListEl);
+        const productList  = new ProductList('app');
+        productList.render()
     }
 }
 
-
 // creating a static class
 class App {
-
     static cart;
 
     static init() {
@@ -199,4 +190,3 @@ class App {
 }
 
 App.init();
-
