e71d7dab7e48fbfd6eca6cab7c4a4fd9f5144369
Knight <mayanks365@gmail.com>
2020-12-07 20:40:16 +0530

    updating querySelector

--- Dom/getElementBy/querySelector/app.js
+++ Dom/getElementBy/querySelector/app.js
@@ -1,7 +1,7 @@
 // ****************
 // querySelector
 // ****************
-//To find the first li on the page:
+// To find the first li on the page:
 // two li elements are there
 // querySelector will select first element
 const liList = document.querySelector('li');
@@ -9,7 +9,7 @@ console.log(liList);
 
 // 
 // selecting class with dot
-//To find the first element with class of special:
+// To find the first element with class of special:
 const splClass = document.querySelector('.special');
 console.log(splClass)
 
@@ -21,12 +21,30 @@ console.log(idMain);
 
 // To find the first li with the class of special, nested inside of a ul, nested inside a section:
 const nstd = document.querySelector('section ul li.special');
-console.log(nstd)
+console.log(nstd) //  <li class="special"></li>
+
+// querySelector inside querySelector
+const ul = document.querySelector('ul');
+console.log(ul.querySelector('li'))
+
+// 
+// searching for child node with 
+// getElementById // only getElement which can be used
+// then querySelector -> can only use querySelector
+// for id and class
+const n = document.getElementById('unOrd-list')
+console.log(n) // <ul id="unOrd-list"> <ul>
+console.dir(n.querySelector('li#carrot')) // li#carrot
+console.dir(n.querySelector('.special')) // li.special
+
 
 // 
 // css atribute selector syntax
 const inp = document.querySelector('input[type="password"]')
 console.log(inp);
+
+const sp = document.querySelector('section ul li:first-of-type')
+console.log(sp) // <li id="carrot">Carrots</li>
 
 // ****************
 // querySelectorAll
@@ -42,3 +60,4 @@ console.log(ALLspecial);
 
 console.log(ALLspecial[0])
 console.log(ALLspecial.length)
+
