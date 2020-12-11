199294998d580e075fdb7c0d22e266a58deac85c
Knight <mayanks365@gmail.com>
2020-12-11 19:51:04 +0530

    updating dynamic property folder

---
+++ Objects/dynamicPropertyAccess/apps.js
@@ -0,0 +1,33 @@
+// dynamic keyname must be declared before they are called on object
+const username = 'User'
+
+let person = {
+    // key with spaces can also be denifed 
+    // but needs to be kept in quotes 
+    'first name': 'Max',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+
+    greet: function() {
+        alert('Hi there!');
+    },
+
+    // key can also be defined as number
+    // number must be >=0 , positive intiger
+    1.1 : 'yoo',
+
+    // dynamically setting the property
+    // value inside square braket are NOT taken as key name
+    // it will search for variable of that name
+    // and take value i.e 'User' stored in that as a key name which is added to object person
+    [username] : "", // username = 'User'
+};
+
+const keyName = 'first name'
+
+// dynamically accesing the properties
+console.log(person[keyName])
+person.User = 'Knight'
+console.log(person.User)
+
+console.log(person)
