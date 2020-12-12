6497928589c79a649d578938004d74e5b80ac5c5
Knight <mayanks365@gmail.com>
2020-12-12 20:33:56 +0530

    updating object folder

--- Objects/movie/assets/scripts/objects.js
+++ Objects/movie/assets/scripts/objects.js
@@ -1,7 +1,40 @@
 const addMovieBtn = document.getElementById('add-movie-btn')
 const searchBtn = document.getElementById('search-btn')
-
 const movies = [];
+
+// const clearInput = () => {
+//     title = '';
+//     extraName = '';
+//     extraValue = ''
+// }
+
+const renderMovies = ( filter = '' ) => {
+  const movieList = document.getElementById('movie-list');
+
+  if( movies.length === 0 ){
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  const filteredMovies = !filter ? movies 
+  : movies.filter( movie => movie.info.title.includes(filter.toLowerCase()) )
+
+  filteredMovies.forEach( (movie) => {
+    const movieEl = document.createElement('li');
+    let text = movie.info.title ;
+    for ( const key in movie.info ){
+      if( key !== 'title'){
+        text = text + ` ${key}: ${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movieList.append(movieEl)
+  });
+// clearInput();
+}
 
 const addMovieHandler = () => {
   const title = document.getElementById('title').value
@@ -25,6 +58,18 @@ const addMovieHandler = () => {
   };
   movies.push(newMovie);
   console.log(newMovie)
+  renderMovies();
+
+  title === "";
+  extraName === "";
+  extraValue === ""
+
 };
 
+const searchMovieHandler = ()=> {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies( filterTerm );
+}
+
 addMovieBtn.addEventListener( 'click', addMovieHandler );
+searchBtn.addEventListener( 'click', searchMovieHandler )
