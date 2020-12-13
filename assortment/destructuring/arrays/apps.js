66ad392e408bb8dc9990437149eebde0147b6236
Knight <mayanks365@gmail.com>
2020-12-13 20:35:53 +0530

    updating assortment folder

--- assortment/destructuring/arrays/apps.js
+++ assortment/destructuring/arrays/apps.js
@@ -14,19 +14,19 @@ const raceResults = [
 ]
 // creating variables for first 3
 // order is important
-const [gold, silver, bronze]= raceResults;
-console.log(gold)
-console.log(silver)
-console.log(bronze)
+const [gold, silver, bronze] = raceResults;
+console.log(`Gold goes to -- ${gold}`)
+console.log(`Silver goes to -- ${silver}`)
+console.log(`Bronze goes to -- ${bronze}`)
 // 
 // 
 // selecting 1st and 4th result
-const[ first,,,fourth]= raceResults;
-console.log(first)
-console.log(fourth)
+const[ first, , ,fourth]= raceResults;
+console.log(`First is -- ${first}`)
+console.log(`Fourth is -- ${fourth}`)
 // 
 // selecting 1st as Winner 2nd as refree remaining as other
 
 const[ winner, second, ...others]= raceResults;
-console.log(winner, second)
-console.log(others)
+console.log(`Winner is ${winner}. ${second} was on Second Position`)
+console.log(`While others are ${others}`)
--- assortment/spread/apps.js
+++ assortment/spread/apps.js
@@ -33,3 +33,46 @@ console.log(catDog)
 //  value will be updated
 const dog = { ...canine, isPet:true }
 console.log(dog)
+
+// //////////////////////////////////////////
+// creating new object with spread operator /
+// //////////////////////////////////////////
+
+const person = {name: 'Knight', hobbies: ['Martial Art', 'coding']}
+
+const newPerson = person;
+
+// updating the person will also update the newPerson
+person.hobbies[1] = 'Web Development'
+console.log(person)
+console.log(newPerson)
+
+// as the above method use reference to create new object
+// so when you update the original object, 
+// newly created object is also updated
+
+// ///////////////////////////////////
+// using spread to create new object /
+// ///////////////////////////////////
+const personTwo = { ...person }
+console.log(personTwo)
+// updating person will not show newly made changes
+// in object created with spread
+person.name = 'Luffy' // changing name
+console.log(personTwo) // name remain same in new object
+
+// as person object has hobbies array
+// you need to apply spread to that array too 
+// so that changes made in person not 
+// shown in new object
+const personThree = { ...person, name: 'Babu Rao'} // overwriting name
+console.log(personThree)
+person.hobbies.pop()
+console.log(person)
+console.log(personThree)
+// creating new array that will clone the old array
+const personFour = { ...person, hobbies: [...person.hobbies] }
+console.log(personFour)
+person.hobbies[1] = 'Coding' //updating old
+console.log(person)
+console.log(personFour) // changes will not affect new object
