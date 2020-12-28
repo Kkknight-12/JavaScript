e22d4a82ce76341f8e022819c5271884f53fadd2
Knight <mayanks365@gmail.com>
2020-12-28 23:33:01 +0530

    practice

---
+++ regEx/basic/apps.js
@@ -0,0 +1,9 @@
+/* 
+The includes() method determines whether a string 
+contains the characters of a specified string.
+This method returns true if the string contains 
+the characters, and false if not.
+ */
+const userInput = 'test.com'
+console.log(userInput.includes('@'))
+console.log(userInput.includes('.'))
