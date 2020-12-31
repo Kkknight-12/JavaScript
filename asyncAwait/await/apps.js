465ceb8210be11623d78b62d636de2802122035a
Knight <mayanks365@gmail.com>
2020-12-31 15:22:37 +0530

    updating async

--- asyncAwait/await/apps.js
+++ asyncAwait/await/apps.js
@@ -21,3 +21,95 @@ async function getPlanets() {
     console.log(res.data)
 }
 getPlanets()
+
+// 
+// 
+function resolveAfter2Seconds() {
+  res =  new Promise( resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+  return res;
+}
+
+async function asyncCall() {
+  console.log('calling');
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  // expected output: "resolved"
+}
+
+asyncCall();
+
+// ///////
+const button = document.querySelector('button');
+
+const getPosition = ( opts ) => {
+    const promise = new Promise( ( resolve, reject ) => {
+        navigator.geolocation.getCurrentPosition(
+            success => {
+                resolve( success ); 
+            },
+            error => {
+                reject( error ) 
+            },
+            // opts
+        );
+    });
+    return promise;
+}
+
+const setTimer = ( duration ) => { 
+    const promise = new Promise( ( resolve, reject ) => {
+        setTimeout( () => {
+            resolve( 'Done!' );
+        }, duration );
+    });
+    return promise;
+}
+
+/* 
+async allow us to ommit .then() and .catch() method and make your code look more like 
+async code. you can use Async await only in functions which you enable it by writting async keyword in front of your function keyword, on expression you write in front of your equal sign 
+const setTimer = async ( duration ) => { }
+
+with async in front of funciton automatically returns  promise
+ */
+async function trackUserHandler() {
+    // let positionData;
+    // getPosition();
+    let posData,
+    timerData;
+    /* 
+    now you have access to await keyword which you can 
+    write in front of any promise 
+    only when the first promise is resolved the other promsie will execute 
+    
+    getPosition and setTimer are part of trackUserHandler's one big promise
+    */
+    try {
+        posData = await getPosition();
+        timerData = await setTimer(2000);
+    } 
+    catch(error) {
+        console.log(error);
+    }
+    console.log(timerData, posData)
+        // .then( posData => { 
+        //     positionData = posData; 
+        //     console.log('Setting timer')
+        //     return setTimer(2000); 
+        // }) 
+        // .then( data => { 
+        //     console.log( data, positionData ); 
+        // })
+        // .catch( err => {
+        //     console.log( err )
+        // }) 
+    // setTimer(1000).then( () => { 
+    //     console.log( 'Timer done!' )
+    // });
+    console.log( 'Getting position...' )
+}
+button.addEventListener( 'click', trackUserHandler );
--- asyncAwait/refactoringPromsie.all/apps.js
+++ asyncAwait/refactoringPromsie.all/apps.js
@@ -17,3 +17,84 @@ function printPokemon( results ) {
     }
 }
 get3Pokemon()
+
+// 
+// 
+// 
+const button = document.querySelector('button');
+
+const getPosition = ( opts ) => {
+    const promise = new Promise( ( resolve, reject ) => {
+        navigator.geolocation.getCurrentPosition(
+            success => {
+                // passing the data(success: GeolocationPosition) to resolve object
+                resolve( success ); 
+            },
+            error => {
+                reject( error ) // passing error object
+            },
+            // opts
+        );
+    });
+    return promise;
+}
+
+const setTimer = ( duration ) => { // wait for 2sec from line 59
+    const promise = new Promise( ( resolve, reject ) => {
+        setTimeout( () => {
+            resolve( 'Done!' );
+        }, duration );
+    });
+    return promise;
+}
+
+// chaining .then()
+async function trackUserHandler() {
+    let posData,
+    timerData;
+    try {
+        posData = await getPosition();
+        timerData = await setTimer(1000);
+    } 
+    catch(error) {
+        console.log(error);
+    }
+    console.log(timerData, posData)
+    console.log( 'Getting position...' )
+}
+
+button.addEventListener( 'click', trackUserHandler );
+
+
+// .race() compare the two promise and return the 
+// one that perform faster then the other
+Promise.race( [getPosition(), setTimer(1000)])
+    .then( data => {
+        console.log(data)
+})
+
+// either all promise run or it fail
+// promise.all returns an list
+// all promises run and the combined data is returned
+Promise.all( [getPosition(), setTimer(1000)] )
+    .then( promiseData => { 
+        console.log(promiseData)
+})
+/* 
+if one of the promises fail then other promise is not executed 
+so to get the output all promises must resolve.
+But for rejection, even if one promise reject other
+promises listed after it are not excuted and we just get
+the error, which we can handle with catch block.
+*/
+
+/* 
+provide more detailed output as
+execution does't stop of other promises 
+if a promise is rejected instead it run all of them
+and then give detailed summary of which promise fail
+and which succeed */
+Promise.allSettled([getPosition(), setTimer(1000)] )
+    .then( promiseData => { 
+        console.log(promiseData)
+})
