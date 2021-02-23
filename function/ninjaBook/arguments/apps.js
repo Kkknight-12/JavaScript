60ec9fe555cf97101f58662c1a875e9a43bc2c28
Knight <mayanks365@gmail.com>
2021-02-23 23:33:01 +0530

    practice

--- function/ninjaBook/arguments/apps.js
+++ function/ninjaBook/arguments/apps.js
@@ -30,7 +30,7 @@ function hide(ninja) {
   ninja.visibility = false;
   return ninja;
 }
-console.log(hide({}));
+console.log(hide({})); // {visibility: false}
 
 // can be returned as values
 function returnNewNinja() {
@@ -38,10 +38,12 @@ function returnNewNinja() {
 }
 console.log(returnNewNinja());
 
-// functions posses all the capabilities of object
-// can be treated link object
-// called first-class object
-
+console.log('------------------------')
+// //////////////////////////////////////////////////
+// functions posses all the capabilities of object  /
+// can be treated link object                       /
+// called first-class object                        /
+// //////////////////////////////////////////////////
 // Assignment
 var ninjaFn = function () {};
 console.log(ninjaFn);
@@ -55,7 +57,7 @@ console.log(ninjaArray);
 // can be assigned as a property of object
 var ninja = {};
 ninja.data = function () {};
-console.log(ninja);
+console.log(ninja); // {data: ƒ}
 
 // can be passed as an argument
 function call(ninjafunction) {
@@ -81,19 +83,20 @@ console.log(returnSamuraiFun());
 // created and assigned;
 var ninjafunction = function() {};
 ninjafunction.ninja = "shadow clone jutsu";
-console.log(ninjafunction.ninja);
+console.log(ninjafunction.ninja); // shadow clone jutsu
 
-// callback
+// ////////////
+// callback  / 
+// ///////////
 var text = 'you reached call back';
-function useless(ninjaCallback) {
-  return ninjaCallback();
-}
-
 function getText(){
   return text;
 }
-
+function useless(ninjaCallback) {
+  return ninjaCallback();
+}
 console.log(useless(getText))
+
 // rewrite callback
 var text2 = 'you again reached call back';
 function useless2( callback ){
@@ -107,23 +110,30 @@ var values  =[0,3,6,7,2,1,9,3]
 values.sort(function (val1, val2 ) {
   return val2 - val1;
 });
-console.log(values)
+console.log(values) // [9, 7, 6, 3, 3, 2, 1, 0]
+
+// //////////////////////////////////
+// Attaching properties to function /
+// //////////////////////////////////
 
 var weildSword = function(){};
 weildSword.swordType = "katana";
-console.log(weildSword)
+console.log(weildSword.swordType) // katana
 
+// storing function in collection
 var store = {
     nextId: 1,
     cache: {},
     add: function(fn){
-        if( !fn.id ){ // if function don't have id method then proceed
+        if( !fn.id ){ 
+        // if function don't have id property then proceed
         // store the current value of nextId to function.id 
         // then increment the current value of nextId 
         // knight.id = 1, netninja.id = 2
-            fn.id = this.nextId++; 
+            fn.id = this.nextId++;  // here adding property to function
+            fn.age = new Date().getTime().toString() 
             // now save function as key and id as value {1: ƒ, 2: ƒ}
-            this.cache[fn.id] = fn;
+            this.cache[fn.id] = fn; // here we are adding funciton to cache object
             return true;
         }
     }
@@ -132,10 +142,14 @@ function knight( ){return 'this is knight'};
 store.add(knight)
 // assert( store.add(knight), "Function was safely added.");
 // assert( store.add(knight), "but it was only added once.");
-
 store.add( function netninja(){})
 console.log(store.cache) // {1: ƒ, 2: ƒ}
+console.log(store.cache[1]())
+console.log(store.cache[1].id)
 
+// //////////////////////////
+// Self-memoizing functions /
+// //////////////////////////
 function isPrime(value){
 
     if (!isPrime.answers){
@@ -145,10 +159,10 @@ function isPrime(value){
     if( isPrime.answers[value] !== undefined ){
         return isPrime.answer[value];
     }
-    // will return true if value is neither 0 or 1
+    // will set prime to true if value is neither 0 or 1
     var prime = value !==0 && value !==1;
 
-    for( var i = 2; i< value; i++ ){
+    for( var i = 2; i < value; i++ ){
         if(value % i === 0 ){
             prime = false;
             break;
@@ -159,7 +173,3 @@ function isPrime(value){
 }
 console.log(isPrime(5));
 console.log(isPrime.answers) //{5: true}
-
-let va = {make: "Ford", model: "Mustang", year: 1969}
-va['name'] = 'fake';
-console.log(va)
