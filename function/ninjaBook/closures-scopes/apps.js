671287630c745faa17221d15432e42dfa1044469
Knight <mayanks365@gmail.com>
2021-03-27 18:03:40 +0530

    practice

--- function/ninjaBook/closures-scopes/apps.js
+++ function/ninjaBook/closures-scopes/apps.js
@@ -7,7 +7,7 @@ Scope is part of the program in which a certain name is bound to a certain varia
 
 // global variables
 var outerValue = 'Knight';
-var later;
+var later; // innerFunction()
 
 function outerFunction(){
   var innerValue = 'ninja'; // local variable
@@ -23,12 +23,38 @@ function outerFunction(){
   later = innerFunction // creating refernce to global variable
   // innerFunction();
 }
-outerFunction()
-later();
+outerFunction(); // -> local innerFunction, innervalue, this- window
+later(); // local -> this - window | Closure(outerFunction)-> innervalue
 
 /*
 we are executing 'innerFunction' after 'outerFunction' has been executed via
 trick-> copying a referencing to the function to global variable 'later'. 
-When innerFunction executes, scope inside outerfunction is long gone. But 'innervalue'
+When innerFunction executes, scope inside outerfunction is long gone. But because we have 'closure' the 'innervalue' variable will be available when we execute inner function.
+
+When we declare innerFunction inside the outer function, not only is the function
+declaration defined, but the closure is created that encompasses the function
+definition as we as ll variables in scope at the point of function definition
 
  */
+
+function one() {
+  two();
+  function two() {
+    three();
+    function three() {
+      // function three's code here
+    }
+  }
+}
+
+one();
+
+const symbol = '¥';
+
+function displayPrice(price) {
+  const symbol = '$';
+  console.log(symbol + price);
+}
+
+displayPrice('80');
+// '$80'
