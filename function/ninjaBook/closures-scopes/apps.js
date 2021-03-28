faebbe47c12aeb40680e661c3c730d0d52aa65e0
Knight <mayanks365@gmail.com>
2021-03-28 18:02:51 +0530

    practice

--- function/ninjaBook/closures-scopes/apps.js
+++ function/ninjaBook/closures-scopes/apps.js
@@ -85,7 +85,7 @@ introduceMyself();
 myName is a variable defined outside a function, hence it's a global variable in the global scope. In other words, myName is available for all functions to use.
 But let's look closely at the other variable: you and str. you and str is referenced by introduce(), even though it wasn't declared within introduce()! This is possible because a nested function's scope includes variables declared in the scope where the function is nested (i.e., variables from its parent function's scope, where the function is defined).
 As it turns out, the introduce() function and its lexical environment form a closure. This way, introduce() has access to not only the global variable myName, but also the variable you and str, which was declared in the scope of its parent function, introduceMyself() and inner().
- */
+*/
 
 function outerFunction2() {
   let num1 = 5;
@@ -99,7 +99,8 @@ let result = outerFunction2();
 
 result(10);
 
-/* After outerFunction2() is returned, it may seem that all of its local variables would be allocated back to available memory. As it turns out, however, the nested innerFunction() still has access to the num1 variable!
+/* 
+After outerFunction2() is returned, it may seem that all of its local variables would be allocated back to available memory. As it turns out, however, the nested innerFunction() still has access to the num1 variable!
 
 Let's take a closer look: outerFunction2() returns a reference to the inner, nested function. The return value of this invocation is saved in result. When this function is called, it maintains access to its scope; that is, all the variables it was able to access back when it was originally defined. This includes the num1 variable in its parent scope. The nested function closes over these variables, and those variables persist as long as the reference to the function itself exists.
 
