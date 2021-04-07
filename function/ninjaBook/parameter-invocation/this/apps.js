c1b1672f8841f3e956a63232488e4a8640fed7a8
Knight <mayanks365@gmail.com>
2021-04-07 11:01:01 +0530

    practice

--- function/ninjaBook/parameter-invocation/this/apps.js
+++ function/ninjaBook/parameter-invocation/this/apps.js
@@ -371,14 +371,3 @@ console.log(bindbt) // {clicked: false, click: ƒ}
 
 
 // ////////////////////////
-var index = [
-    {key: 1, sector: 10, t_score: 18, id: '1236n7e8', value: 'Klingon'},
-    {key: 4, sector: 145, t_score: 12, id: '293847hs8', value: 'Minbari'},
-    {key: 8, sector: 214, t_score: 5, id: '283hy8347', value: 'Cylon'},
-    {key: 3, sector: 8346, t_score: 10, id: 'n9837ks857', value: 'Jawa'},
-]
-var newIndex = []
-index.map( (item) => {
-    newIndex.push({'id': item.id.slice(0,3) , 'value': item.value})
-})
-console.log(newIndex)
