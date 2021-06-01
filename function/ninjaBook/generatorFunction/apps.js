5dc969ec0dcfa903c8155e3c63b0245795e4e8c8
Knight <mayanks365@gmail.com>
2021-06-01 10:01:11 +0530

    practice

--- function/ninjaBook/generatorFunction/apps.js
+++ function/ninjaBook/generatorFunction/apps.js
@@ -11,3 +11,36 @@ for(let weapon of WeaponGenerator()) {
 }
 
 
+// example 2
+function* WeaponGenerator2(){
+  yield "Katana";
+  yield "Wakizashi";
+}
+
+const weaponsIterator = WeaponGenerator2();
+
+// next function
+const result1 = weaponsIterator.next();
+console.log(result1) // {value: "Katana", done: false}
+
+if(typeof result1 === "object"
+    && result1.value === "Katana"
+    && !result1.done){
+        console.log("Katana received!")
+}
+
+const result2 = weaponsIterator.next();
+if(typeof result2 === "object"
+    && result2.value === "Wakizashi"
+    && !result2.done){
+        console.log( "Wakizashi received!")
+}
+
+const result3 = weaponsIterator.next();
+if(typeof result3 === "object"
+    && result3.value === undefined
+    && result3.done){
+        console.log("There are no more results!")
+}
+
+
