3b8937595cb72d47cb9ec50c5728964875a5c303
Knight <mayanks365@gmail.com>
2021-07-10 15:01:10 +0530

    practice

--- asynchronousCallbackPromises/ninjaBook/WorkingWithPromises/apps.js
+++ asynchronousCallbackPromises/ninjaBook/WorkingWithPromises/apps.js
@@ -14,35 +14,44 @@
 
 // 
 // Example
-console.log("At code start"); // 1
+// console.log("At code start"); // 1
 
-var ninjaDelayedPromise = new Promise((resolve, reject) => {
-  console.log("ninjaDelayedPromise executor"); // 2
-  setTimeout(() => {
-    console.log("Resolving ninjaDelayedPromise"); // 7
-    resolve("Hattori");
-  }, 500);
+// var ninjaDelayedPromise = new Promise((resolve, reject) => {
+//   console.log("ninjaDelayedPromise executor"); // 2
+//   setTimeout(() => {
+//     console.log("Resolving ninjaDelayedPromise"); // 7
+//     resolve("Hattori");
+//   }, 500);
+// });
+
+// if(ninjaDelayedPromise !== null){
+//     console.log("After creating ninjaDelayedPromise") // 3
+// } 
+
+// ninjaDelayedPromise.then(ninja => {
+//   if(ninja === "Hattori"){
+//     console.log("ninjaDelayedPromise resolve handled with Hattori"); // 8
+//   }
+// });
+
+// const ninjaImmediatePromise = new Promise((resolve, reject) => {
+//   console.log("ninjaImmediatePromise executor. Immediate resolve."); // 4
+//   resolve("Yoshi");
+// });
+
+// ninjaImmediatePromise.then(ninja => {
+//   if(ninja === "Yoshi"){
+//     console.log("ninjaImmediatePromise resolve handled with Yoshi"); // 6
+//   }
+// });
+
+// console.log("At code end"); // 5
+
+const promise = new Promise((resolve, reject) => {
+  reject("Explicitly reject a promise!");
 });
 
-if(ninjaDelayedPromise !== null){
-    console.log("After creating ninjaDelayedPromise") // 3
-} 
-
-ninjaDelayedPromise.then(ninja => {
-  if(ninja === "Hattori"){
-    console.log("ninjaDelayedPromise resolve handled with Hattori"); // 8
-  }
-});
-
-const ninjaImmediatePromise = new Promise((resolve, reject) => {
-  console.log("ninjaImmediatePromise executor. Immediate resolve."); // 4
-  resolve("Yoshi");
-});
-
-ninjaImmediatePromise.then(ninja => {
-  if(ninja === "Yoshi"){
-    console.log("ninjaImmediatePromise resolve handled with Yoshi"); // 6
-  }
-});
-
-console.log("At code end"); // 5
+promise.then(
+  () => console.log("Happy path, won't be called!"),
+  error => console.log("A promise was explicitly rejected!")
+);
